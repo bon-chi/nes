d5a225011182a3c1d38d4050cd924a2ca4882b52
Koji Ota <afterjnih@gmail.com>
2018-02-18 04:32:18 +0900

    tmp

--- src/nes/ppu/mod.rs
+++ src/nes/ppu/mod.rs
@@ -13,6 +13,8 @@ use nes::ppu::piston_window::Button::Keyboard;
 use nes::ppu::piston_window::Key;
 use nes::ppu::glutin_window::GlutinWindow as Window;
 use self::opengl_graphics::{GlGraphics, OpenGL};
+use self::graphics::Rectangle;
+use self::graphics::types::Color;
 use nes::cpu::Cpu;
 use nes::cpu::PrgRam;
 use std::sync::{Arc, Mutex};
@@ -22,39 +24,83 @@ pub struct Ppu2 {
 
     //background
     v_ram: Box<VRam>,
-    vram_address_register: VramAddressRegister,
-    temporary_vram_address: VramAddressRegister, // yyy, NN, YYYYY, XXXXX
+    v_ram_address_register: VRamAddressRegister,
+    temporary_v_ram_address: VRamAddressRegister, // yyy, NN, YYYYY, XXXXX
     fine_x_scroll: u8,
     first_or_second_write_toggle: bool,
-    background_shift_register16_0: (u8, u8),
-    background_shift_register16_1: (u8, u8),
-    background_shift_register8_0: u8,
-    background_shift_register8_1: u8,
+    pattern_high_value_register: (u8, u8),
+    pattern_low_value_register: (u8, u8),
+    attr_value_register: u8,
 
     // vram_index: u16,
     prg_ram: Arc<Mutex<PrgRam>>,
+
+    cycle: u16,
+    line: u16,
 }
 
 impl Ppu2 {
     pub fn new(prg_ram: Arc<Mutex<PrgRam>>) -> Ppu2 {
         Ppu2 {
             v_ram: Box::new(VRam([0; 0xFFFF])),
-            vram_address_register: VramAddressRegister::new(),
-            temporary_vram_address: VramAddressRegister::new(),
+            v_ram_address_register: VRamAddressRegister::new(),
+            temporary_v_ram_address: VRamAddressRegister::new(),
             fine_x_scroll: 0,
             first_or_second_write_toggle: false,
-            background_shift_register16_0: (0, 0),
-            background_shift_register16_1: (0, 0),
-            background_shift_register8_0: 0,
-            background_shift_register8_1: 0,
+            pattern_high_value_register: (0, 0),
+            pattern_low_value_register: (0, 0),
+            attr_value_register: 0,
             prg_ram,
+            cycle: 0,
+            line: 0,
         }
     }
     pub fn run<G: Graphics>(&mut self, c: &Context, g: &mut G) {
-        use self::graphics::Rectangle;
+        // use self::graphics::Rectangle;
         loop {
             // Rectangle::new([0.0, 1.0, 0.0, 1.0]).draw([0.0, 0.0, 85.0, 80.0], &c.draw_state, c.transform, g);
         }
+    }
+    fn get_pattern_value(&self) -> u8 {
+        (self.pattern_high_value_register.1 >> 7) + (self.pattern_low_value_register.1 >> 7) * 2
+    }
+    fn get_palette_num(&self) -> u8 {
+        self.attr_value_register;
+        match (self.cycle - 1) % 4 {
+            0 | 1 => {
+                match self.line % 4 {
+                    0 | 1 => self.attr_value_register & 0b00000011,
+                    _ => (self.attr_value_register >> 4) & 0b0011,
+                }
+            }
+            _ => {
+                match self.line % 4 {
+                    0 | 1 => (self.attr_value_register >> 2) & 0b000011,
+                    _ => (self.attr_value_register >> 6) & 0b11,
+                }
+            }
+        }
+
+    }
+    fn shift_and_draw_pixel<G: Graphics>(&mut self, c: &Context, g: &mut G) {
+        let (color, rectangle) = self.shift_to_pixel();
+        // println!("{:?}, {:?}", color, rectangle);
+        Rectangle::new(color).draw(rectangle, &c.draw_state, c.transform, g);
+    }
+
+    fn shift_to_pixel(&mut self) -> (Color, [f64; 4]) {
+        let color: Color = self.v_ram.get_color(self.v_ram.fetch8(
+            VRam::IMAGE_PALETTE + ((self.get_palette_num() as u16) * 4) +
+                (self.get_pattern_value() as u16),
+        ));
+        self.shift_registers();
+        // println!("{}", self.cycle);
+        (color, [self.cycle as f64 - 1.0, self.line as f64, 1.0, 1.0])
+        // (color, [self.cycle as f64 - 1.0, self.line as f64, 5.0, 5.0])
+    }
+    fn shift_registers(&mut self) {
+        self.pattern_high_value_register.1 = self.pattern_high_value_register.1 << 1;
+        self.pattern_low_value_register.1 = self.pattern_low_value_register.1 << 1;
     }
     pub fn run2(&mut self, txk: Sender<Option<Key>>, rx: Receiver<u8>) {
         use self::graphics::Rectangle;
@@ -67,7 +113,6 @@ impl Ppu2 {
         let mut gl = GlGraphics::new(opengl);
         let mut events = Events::new(EventSettings::new());
         while let Some(e) = events.next(&mut window) {
-            // println!("{}", rx.recv().unwrap());
             rx.recv().unwrap();
             let mut key = None;
             if let Some(button) = e.press_args() {
@@ -82,12 +127,28 @@ impl Ppu2 {
             if let Some(args) = e.render_args() {
                 gl.draw(args.viewport(), |c, g| {
                     use self::graphics::clear;
-                    clear([1.0; 4], g);
-                    Rectangle::new([0.0, 1.0, 0.0, 1.0]).draw([0.0, 0.0, 85.0, 80.0], &c.draw_state, c.transform, g);
-                    Rectangle::new([1.0, 0.0, 0.0, 1.0]).draw([85.0, 80.0, 85.0, 80.0], &c.draw_state, c.transform, g);
-                    Rectangle::new([0.0, 0.0, 1.0, 1.0]).draw([170.0, 160.0, 85.0, 80.0], &c.draw_state, c.transform, g);
+                    // clear([1.0; 4], g);
+                    // loop
+                    // cycle 0
+                    // cycle 1-256
+                    if 1 <= self.cycle && self.cycle <= 256 {
+                        self.shift_and_draw_pixel(&c, g);
+                    }
+                    // cycle 257-320
+                    // Rectangle::new([0.0, 1.0, 0.0, 1.0]).draw([0.0, 0.0, 85.0, 80.0], &c.draw_state, c.transform, g);
+                    // Rectangle::new([1.0, 0.0, 0.0, 1.0]).draw([85.0, 80.0, 85.0, 80.0], &c.draw_state, c.transform, g);
+                    // Rectangle::new([0.0, 0.0, 1.0, 1.0]).draw([170.0, 160.0, 85.0, 80.0], &c.draw_state, c.transform, g);
                     // tv.draw(&nes_controller, &c, g);
                 });
+                self.cycle += 1;
+                if self.cycle >= 341 {
+                    self.cycle = 0;
+                    self.line += 1;
+                    if self.line >= 261 {
+                        self.line = 0;
+                    }
+                }
+            } else {
             }
             txk.send(key);
         }
@@ -141,8 +202,8 @@ pub struct Ppu {
     spr_counter7: u8,
 
     //background
-    vram_address_register: VramAddressRegister,
-    temporary_vram_address: VramAddressRegister, // yyy, NN, YYYYY, XXXXX
+    vram_address_register: VRamAddressRegister,
+    temporary_vram_address: VRamAddressRegister, // yyy, NN, YYYYY, XXXXX
     fine_x_scroll: u8,
     first_or_second_write_toggle: bool,
     // first/second write toggle
@@ -226,7 +287,7 @@ impl Ppu {
                 let background1 = Self::pixel_bit(self.background_shift_register16_0.1);
                 let background2 = Self::pixel_bit(self.background_shift_register16_1.1) * 2;
                 let background = background1 + background2;
-                Rectangle::new([0.0, 1.0, 0.0, 1.0]).draw([0.0, 0.0, 85.0, 80.0], &c.draw_state, c.transform, g);
+                // Rectangle::new([0.0, 1.0, 0.0, 1.0]).draw([0.0, 0.0, 85.0, 80.0], &c.draw_state, c.transform, g);
                 // shift
                 self.shift_pixel_bit();
 
@@ -311,6 +372,9 @@ pub struct SecondarySprRam([u8; 0x1F]);
 
 pub struct VRam([u8; 0xFFFF]);
 impl VRam {
+    const PATTERN_TABLE0: u16 = 0x0000;
+    const PATTERN_TABLE1: u16 = 0x1000;
+
     const NAME_TABLE0: u16 = 0x2000;
     const NAME_TABLE1: u16 = 0x2400;
     const NAME_TABLE2: u16 = 0x2800;
@@ -321,8 +385,8 @@ impl VRam {
     const ATTR_TABLE2: u16 = 0x2BC0;
     const ATTR_TABLE3: u16 = 0x2FC0;
 
-    const PATTERN_TABLE0: u16 = 0x0000;
-    const PATTERN_TABLE1: u16 = 0x1000;
+    const IMAGE_PALETTE: u16 = 0x3F00;
+    const SPRITE_PALETTE: u16 = 0x3F10;
 
     fn get_name_table_value(&self, table_num: u8, index: u16) -> u8 {
         match table_num {
@@ -367,18 +431,21 @@ impl VRam {
         let pattern1 = self.0[pattern_table_idx as usize + 8];
         (pattern0, pattern1)
     }
+    fn get_color(&self, idx: u8) -> Color {
+        Colors::COLORS[idx as usize]
+    }
 }
 
-struct VramAddressRegister {
+struct VRamAddressRegister {
     y_offset_from_scanline: u8,
     name_table_num: u8,
     y_idx: u8,
     x_idx: u8,
 }
 
-impl VramAddressRegister {
-    fn new() -> VramAddressRegister {
-        VramAddressRegister {
+impl VRamAddressRegister {
+    fn new() -> VRamAddressRegister {
+        VRamAddressRegister {
             y_offset_from_scanline: 0,
             name_table_num: 0,
             y_idx: 0,
@@ -397,4 +464,74 @@ impl VramAddressRegister {
         }
         (fine_y_scroll, name_table_num, y_panel_pos, x_panel_pos)
     }
+}
+
+struct Colors;
+impl Colors {
+    const COLORS: [[f32; 4]; 64] = [
+        [0x80 as f32, 0x80 as f32, 0x80 as f32, 1.0],
+        [0x00 as f32, 0x3D as f32, 0xA6 as f32, 1.0],
+        [0x00 as f32, 0x12 as f32, 0xB0 as f32, 1.0],
+        [0x44 as f32, 0x00 as f32, 0x96 as f32, 1.0],
+        [0xA1 as f32, 0x00 as f32, 0x5E as f32, 1.0],
+        [0xC7 as f32, 0x00 as f32, 0x28 as f32, 1.0],
+        [0xBA as f32, 0x06 as f32, 0x00 as f32, 1.0],
+        [0x8C as f32, 0x17 as f32, 0x00 as f32, 1.0],
+        [0x5C as f32, 0x2F as f32, 0x00 as f32, 1.0],
+        [0x10 as f32, 0x45 as f32, 0x00 as f32, 1.0],
+        [0x05 as f32, 0x4A as f32, 0x00 as f32, 1.0],
+        [0x00 as f32, 0x47 as f32, 0x2E as f32, 1.0],
+        [0x00 as f32, 0x41 as f32, 0x66 as f32, 1.0],
+        [0x00 as f32, 0x00 as f32, 0x00 as f32, 1.0],
+        [0x05 as f32, 0x05 as f32, 0x05 as f32, 1.0],
+        [0x05 as f32, 0x05 as f32, 0x05 as f32, 1.0],
+        [0xC7 as f32, 0xC7 as f32, 0xC7 as f32, 1.0],
+        [0x00 as f32, 0x77 as f32, 0xFF as f32, 1.0],
+        [0x21 as f32, 0x55 as f32, 0xFF as f32, 1.0],
+        [0x82 as f32, 0x37 as f32, 0xFA as f32, 1.0],
+        [0xEB as f32, 0x2F as f32, 0xB5 as f32, 1.0],
+        [0xFF as f32, 0x29 as f32, 0x50 as f32, 1.0],
+        [0xFF as f32, 0x22 as f32, 0x00 as f32, 1.0],
+        [0xD6 as f32, 0x32 as f32, 0x00 as f32, 1.0],
+        [0xC4 as f32, 0x62 as f32, 0x00 as f32, 1.0],
+        [0x35 as f32, 0x80 as f32, 0x00 as f32, 1.0],
+        [0x05 as f32, 0x8F as f32, 0x00 as f32, 1.0],
+        [0x00 as f32, 0x8A as f32, 0x55 as f32, 1.0],
+        [0x00 as f32, 0x99 as f32, 0xCC as f32, 1.0],
+        [0x21 as f32, 0x21 as f32, 0x21 as f32, 1.0],
+        [0x09 as f32, 0x09 as f32, 0x09 as f32, 1.0],
+        [0x09 as f32, 0x09 as f32, 0x09 as f32, 1.0],
+        [0xFF as f32, 0xFF as f32, 0xFF as f32, 1.0],
+        [0x0F as f32, 0xD7 as f32, 0xFF as f32, 1.0],
+        [0x69 as f32, 0xA2 as f32, 0xFF as f32, 1.0],
+        [0xD4 as f32, 0x80 as f32, 0xFF as f32, 1.0],
+        [0xFF as f32, 0x45 as f32, 0xF3 as f32, 1.0],
+        [0xFF as f32, 0x61 as f32, 0x8B as f32, 1.0],
+        [0xFF as f32, 0x88 as f32, 0x33 as f32, 1.0],
+        [0xFF as f32, 0x9C as f32, 0x12 as f32, 1.0],
+        [0xFA as f32, 0xBC as f32, 0x20 as f32, 1.0],
+        [0x9F as f32, 0xE3 as f32, 0x0E as f32, 1.0],
+        [0x2B as f32, 0xF0 as f32, 0x35 as f32, 1.0],
+        [0x0C as f32, 0xF0 as f32, 0xA4 as f32, 1.0],
+        [0x05 as f32, 0xFB as f32, 0xFF as f32, 1.0],
+        [0x5E as f32, 0x5E as f32, 0x5E as f32, 1.0],
+        [0x0D as f32, 0x0D as f32, 0x0D as f32, 1.0],
+        [0x0D as f32, 0x0D as f32, 0x0D as f32, 1.0],
+        [0xFF as f32, 0xFF as f32, 0xFF as f32, 1.0],
+        [0xA6 as f32, 0xFC as f32, 0xFF as f32, 1.0],
+        [0xB3 as f32, 0xEC as f32, 0xFF as f32, 1.0],
+        [0xDA as f32, 0xAB as f32, 0xEB as f32, 1.0],
+        [0xFF as f32, 0xA8 as f32, 0xF9 as f32, 1.0],
+        [0xFF as f32, 0xAB as f32, 0xB3 as f32, 1.0],
+        [0xFF as f32, 0xD2 as f32, 0xB0 as f32, 1.0],
+        [0xFF as f32, 0xEF as f32, 0xA6 as f32, 1.0],
+        [0xFF as f32, 0xF7 as f32, 0x9C as f32, 1.0],
+        [0xD7 as f32, 0xE8 as f32, 0x95 as f32, 1.0],
+        [0xA6 as f32, 0xED as f32, 0xAF as f32, 1.0],
+        [0xA2 as f32, 0xF2 as f32, 0xDA as f32, 1.0],
+        [0x99 as f32, 0xFF as f32, 0xFC as f32, 1.0],
+        [0xDD as f32, 0xDD as f32, 0xDD as f32, 1.0],
+        [0x11 as f32, 0x11 as f32, 0x11 as f32, 1.0],
+        [0x11 as f32, 0x11 as f32, 0x11 as f32, 1.0],
+    ];
 }
