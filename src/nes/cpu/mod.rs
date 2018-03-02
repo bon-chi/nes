abfb4169395ad6dc9a502b36f35d90cb3036d4a8
Koji Ota <afterjnih@gmail.com>
2018-03-02 22:48:35 +0900

    save

--- src/nes/cpu/mod.rs
+++ src/nes/cpu/mod.rs
@@ -8,6 +8,9 @@ use nes::ppu::VRam;
 /// [CPU](http://wiki.nesdev.com/w/index.php/CPU)
 pub struct Cpu {
     /// [Registers](http://wiki.nesdev.com/w/index.php/CPU_registers)
+    // Indexe Register
+    x: u8,
+    y: u8,
     // Program Counter
     pc: u16,
 
@@ -17,6 +20,8 @@ pub struct Cpu {
 impl Cpu {
     pub fn new(prg_ram: PrgRam) -> Cpu {
         Cpu {
+            x: 0,
+            y: 0,
             pc: 0x8000,
             prg_ram,
         }
@@ -43,6 +48,10 @@ impl Cpu {
     fn parse_instruction(instruction: u8) -> Result<(OpCode, AddressingMode, Option<IndexRegister>)> {
         let (op_code, addressing_mode, register): (OpCode, AddressingMode, Option<IndexRegister>) = match instruction {
             0x78 => (OpCode::SEI, AddressingMode::Implied, None),
+            0x8d => (OpCode::STA, AddressingMode::Absolute, None),
+            0x9a => (OpCode::TXS, AddressingMode::Implied, None),
+            0xa2 => (OpCode::LDX, AddressingMode::Immediate, None),
+            0xa9 => (OpCode::LDA, AddressingMode::Immediate, None),
             _ => Err(format!("unknown instruction: {:0x}", instruction,))?,
         };
         Ok((op_code, addressing_mode, register))
@@ -59,6 +68,34 @@ impl Cpu {
     fn get_operand(&mut self, addressing_mode: AddressingMode, register: Option<IndexRegister>) -> Result<Option<Operand>> {
         let operand: Option<Operand> = match addressing_mode {
             AddressingMode::Implied => None,
+            AddressingMode::Immediate => {
+                let operand = self.prg_ram_value();
+                self.increment_pc();
+                Some(Operand::Value(operand))
+            }
+            AddressingMode::Absolute => {
+                match register {
+                    Some(IndexRegister::X) => {
+                        let address0 = self.prg_ram_value();
+                        self.increment_pc();
+                        let address1 = self.prg_ram_value();
+                        self.increment_pc();
+                        let address = PrgRam::concat_addresses(address1, address0) + self.x as u16;
+                        Some(Operand::Index(address))
+                    }
+                    None => {
+                        let address0 = self.prg_ram_value();
+                        self.increment_pc();
+                        let address1 = self.prg_ram_value();
+                        self.increment_pc();
+                        let address = PrgRam::concat_addresses(address1, address0) as u16;
+                        Some(Operand::Index(address))
+                    }
+                    _ => {
+                        panic!("invalid register: {:?}, addressing_mode is {:?}", register, addressing_mode);
+                    }
+                }
+            }
             _ => Err(format!("invalid AddressingMode: {:?}", addressing_mode))?,
         };
         Ok(operand)
@@ -67,6 +104,7 @@ impl Cpu {
     pub fn get_pc(&self) -> u16 {
         self.pc
     }
+
 
     #[allow(dead_code)]
     pub fn dump(&self) {
@@ -94,6 +132,10 @@ pub struct PrgRam {
 impl PrgRam {
     pub fn new(memory: Box<[u8; 0x10000]>, v_ram: Arc<Mutex<VRam>>) -> PrgRam {
         PrgRam { memory, v_ram }
+    }
+
+    pub fn concat_addresses(address0: u8, address1: u8) -> u16 {
+        ((address0 as u16) << 0b1000) + address1 as u16
     }
 
     pub fn dump(&self) {
